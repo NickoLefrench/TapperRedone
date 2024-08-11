59d3dbe44b86f398d49aec04e48f304c8a53a1af
NickoLefrench <73069093+NickoLefrench@users.noreply.github.com>
2024-08-10 21:53:29 -0400

    added scripts for further interaction
    Player should be able to interact with the beertap, have a beer put into their inventory and drop it elsewhere

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    //this class is to manage the items the player can pick up
+    //player should only be able to pick up 2 items at a time
+    //player can carry only 1 drink at a time
+
+    public List<Item> items = new List<Item>();
+    public GameObject itemPrefab; // Assign this in the inspector
+
+    public void AddItem(Item item)
+    {
+        items.Add(item);
+        Debug.Log("Item added: " + item.itemName);
+    }
+
+    public void DropItem(Item item, Vector3 dropPosition)
+    {
+        // Remove the item from the inventory
+        items.Remove(item);
+
+        // Instantiate the item prefab at the drop position
+        GameObject droppedItem = Instantiate(itemPrefab, dropPosition, Quaternion.identity);
+
+        //how do i ensure its the right dopped item?
+        //If player give wrong drink to cx, then no points/money should be allocated
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Item.cs
+[System.Serializable]
+public class Item
+{
+    public string itemName;
+    public Sprite itemIcon; // Optionally use an icon
+}
+
