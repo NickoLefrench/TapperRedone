6ad0152e67e2ced8a4b7ecc6cc15784f81b4f50e
NickoLefrench <73069093+NickoLefrench@users.noreply.github.com>
2024-08-15 23:29:55 -0400

    Started Score UI
    Started basic frameworks, looked into toolkit vs ugUI

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -6,6 +6,11 @@ public class Score : MonoBehaviour
     int score = 0;
     public Text ScoreText;
 
+    void Start()
+    {
+        ScoreText.text = score.ToString() + " Space Shillings";
+    }
+
     // Update is called once per frame
     void Update()
     {
