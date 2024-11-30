17741b003035b72b837c5a8d495883b51b7a9f0f
NickoLefrench <nicholaslefrancois@gmail.com>
2024-11-30 14:10:04 -0500

    Nothing really changed
    trying different solutions for attaching scoremultiplier to tunable handler
    was distracted so i didnt get far

--- Assets/Scripts/Interactables/CocktailMiniGame.cs
+++ Assets/Scripts/Interactables/CocktailMiniGame.cs
@@ -51,6 +51,8 @@ namespace FMS.TapperRedone.Interactables
         private const string LeftAxis = "Horizontal";
         private const string QuitKey = "Cancel";
 
+        //scoreMultiplier += TunableHandler.Instance.GetScoreMultiplier(); wtf tunable
+
         private void Awake()
         {
             // Null checks moved to Awake
